5d12cfcd6ba35dd7b32a43e7fa6131d15d10641f
Anna <anna.mironova89@yandex.ru>
2024-09-22 14:26:15 +0300

    Add slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+'use client'
 import Slider from '@/components/slider/slider';
 import Link from 'next/link';
+import { useAppSelector } from '@/lib/hooks';
+import { getNews } from '@/lib/news/news-selectors';
 
 export default function Home() {
+  const news = useAppSelector(getNews);
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Slider />
+        <Slider data={news} />
         <Link
           href="/list"
           className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -1,12 +1,51 @@
 'use client';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import Link from 'next/link';
 import styles from './slider.module.css';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { News } from '@/types/news.type';
 
-export default function Slider() {
+type SliderProps = {
+    data: News[];
+}
+
+const SLIDES_PER_VIEW = 3;
+
+export default function Slider({data}: SliderProps) {
     return (
         <div className={styles.container}>
             <Swiper
+            modules={[Navigation, Pagination]}
+            navigation={SLIDES_PER_VIEW < data.length}
+            pagination={
+                SLIDES_PER_VIEW < data.length
+                  ? {
+                      clickable: true,
+                    }
+                  : undefined
+            }
+            slidesPerView={SLIDES_PER_VIEW}
+            >
+                {data.map((item) => (
+                    <SwiperSlide key={item.id} className={styles.slide}>
+                        <Link href={`/${item.id}`}>
+                            <img src={item.image_url} alt={item.title} className={styles.image} />
+                            <p>{item.title}</p>
+                        </Link>
+                    </SwiperSlide>
+                    )
+                )}
+            </Swiper>  
+        </div>     
+    );
+}
+
+/*
+<Swiper
+            modules={[Navigation, Pagination]}
             spaceBetween={50}
             slidesPerView={3}
             pagination={{ clickable: true }}
@@ -14,11 +53,9 @@ export default function Slider() {
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
             >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
+                {data.map((item) => <SwiperSlide key={item.id} className={styles.width}>
+                    
+                    <p className={styles.width}>{item.title}</p>
+                </SwiperSlide>)}
             </Swiper>  
-        </div>     
-    );
-}
+            */
