3b24efb9c26a204aa7a3b8241e2e8bcba76a84dd
Anna <anna.mironova89@yandex.ru>
2024-09-22 15:45:55 +0300

    Add actions for gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/nextjs-test-task",
+    output: "export", 
+};
 
 export default nextConfig;
--- src/lib/news/news-selectors.ts
+++ src/lib/news/news-selectors.ts
@@ -2,3 +2,5 @@ import { News } from '@/types/news.type';
 import { RootState } from '../store';
 
 export const getNews = (state: Pick<RootState, 'news'>): News[] => state['news'].news.data;
+export const getLoadingStatus = (state: Pick<RootState, 'news'>): boolean => state['news'].news.isLoading;
+export const getErrorStatus = (state: Pick<RootState, 'news'>): boolean => state['news'].news.isError;
